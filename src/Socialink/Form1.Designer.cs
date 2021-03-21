f6c4cd77a694e7bfe8c30250d56c8ef701476eaf
riisuki <riisukirs@gmail.com>
2021-03-21 21:09:17 +0700

    basic GUI

--- src/Socialink/Form1.Designer.cs
+++ src/Socialink/Form1.Designer.cs
@@ -29,13 +29,200 @@ namespace Socialink
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DFSbutton = new System.Windows.Forms.RadioButton();
+            this.BFSbutton = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.teksHasil = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(305, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "People You May Know";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Graph File :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Algorithm   :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DFSbutton
+            // 
+            this.DFSbutton.AutoSize = true;
+            this.DFSbutton.Location = new System.Drawing.Point(109, 125);
+            this.DFSbutton.Name = "DFSbutton";
+            this.DFSbutton.Size = new System.Drawing.Size(46, 17);
+            this.DFSbutton.TabIndex = 4;
+            this.DFSbutton.TabStop = true;
+            this.DFSbutton.Text = "DFS";
+            this.DFSbutton.UseVisualStyleBackColor = true;
+            // 
+            // BFSbutton
+            // 
+            this.BFSbutton.AutoSize = true;
+            this.BFSbutton.Location = new System.Drawing.Point(177, 125);
+            this.BFSbutton.Name = "BFSbutton";
+            this.BFSbutton.Size = new System.Drawing.Size(45, 17);
+            this.BFSbutton.TabIndex = 5;
+            this.BFSbutton.TabStop = true;
+            this.BFSbutton.Text = "BFS";
+            this.BFSbutton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "< pilih file >";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Recommend Friends";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Choose Account:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(44, 182);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Explore Friends With:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(44, 251);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Explore";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // teksHasil
+            // 
+            this.teksHasil.BackColor = System.Drawing.SystemColors.Window;
+            this.teksHasil.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.teksHasil.Location = new System.Drawing.Point(407, 90);
+            this.teksHasil.Name = "teksHasil";
+            this.teksHasil.ReadOnly = true;
+            this.teksHasil.Size = new System.Drawing.Size(349, 184);
+            this.teksHasil.TabIndex = 14;
+            this.teksHasil.Text = "< hasil >";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 320);
+            this.Controls.Add(this.teksHasil);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BFSbutton);
+            this.Controls.Add(this.DFSbutton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Socialink";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton DFSbutton;
+        private System.Windows.Forms.RadioButton BFSbutton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox teksHasil;
     }
 }
 
--- src/Socialink/Form1.cs
+++ src/Socialink/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
 
 namespace Socialink
 {
@@ -15,6 +17,93 @@ namespace Socialink
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files|*.txt";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Hapus hasil sebelumnya
+                teksHasil.Text = "";
+
+                // Ubah teks di sebelah browse button
+                label4.Text = dialog.FileName;
+
+                // Baca isi file
+                string isifile = System.IO.File.ReadAllText(label4.Text);
+
+                // Buat daftar huruf/nama akun
+                // ...
+
+                // Ubah dropdown list
+                // ...
+
+                // Buat graf
+                createGraph(isifile);
+
+                // Tampilkan hasil pencarian
+                teksHasil.AppendText("append this");
+            }
+        }
+
+        void DFS(string dari, string ke)
+        {
+            // Jika memilih pencarian DFS
+        }
+
+        void BFS(string dari, string ke)
+        {
+            // Jika memilih pencarian BFS
+        }
+
+        void createGraph(string isifile)
+        {
+            //CONTOH GRAF
+
+            //create a form 
+            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            form.Text = "Graf";
+
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+
+            //create the graph content
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "C");
+            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+
+            //bind the graph to the viewer 
+            viewer.Graph = graph;
+
+            //associate the viewer with the form 
+            form.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            form.Controls.Add(viewer);
+            form.ResumeLayout();
+
+            //show the form 
+            form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
